6935783efd4d8eb2507379e411f1f7aa69a54fbf
drakenshot23 <robertnote23@gmail.com>
2019-05-21 20:54:56 +0200

    Delete CoordinatesRepository.ts

--- src/api/repositories/CoordinatesRepository.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-
-import { Coordinates } from '../models/Coordinates';
-
-@EntityRepository(Coordinates)
-export class CoordinatesRepository extends Repository<Coordinates>  {}
